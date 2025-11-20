3f4767393f5ef3ef4037fb13630341bd46ffc059
govi-nd <gprashar2005@gmail.com>
2025-11-21 01:50:17 +0530

    updated website

---
+++ q2.js
@@ -0,0 +1,13 @@
+let numbers = [23, 5, 89, 12, 45];
+
+let largest = Math.max(...numbers);
+let smallest = Math.min(...numbers);
+
+console.log("Largest: " + largest);
+console.log("Smallest: " + smallest);
+
+let ascending = [...numbers].sort((a, b) => a - b);
+let descending = [...numbers].sort((a, b) => b - a);
+
+console.log("Ascending: " + ascending);
+console.log("Descending: " + descending);
---
+++ q5.js
@@ -0,0 +1,12 @@
+function processArray(arr) {
+  let result = arr
+    .filter(num => num % 2 === 0)   // Remove odd numbers
+    .map(num => num * 2);           // Multiply remaining by 2
+
+  let sum = result.reduce((acc, val) => acc + val, 0); // Sum
+
+  console.log("Processed Array: " + result);
+  console.log("Sum: " + sum);
+}
+
+processArray([1, 2, 3, 4, 5, 6]);
